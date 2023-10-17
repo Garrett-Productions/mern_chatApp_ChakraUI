0c522b2d4a36af0ab3f297ea0ff22f94e0db4ea5
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-17 11:23:41 -1000

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-import {Button} from "@chakra-ui/react"
+// import { Button } from '@chakra-ui/react'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './comps/Homepage';
+import ChatPage from './comps/ChatPage';
+
+
 function App() {
   return (
     <div className="App">
-      <h1><Button colorScheme='blue'>Button</Button></h1>
+      <Routes>
+        <Route path='/' Component={Homepage} exact />
+        <Route path='/chats' Component={ChatPage}/>
+      {/* <Button colorScheme='teal'>Button</Button> */}
+      </Routes>
     </div>
   );
 }
