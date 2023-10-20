8ea7531648e391449343975ef7b5b72e91ef9b36
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 09:24:06 -1000

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './comps/Homepage';
 import ChatPage from './comps/ChatPage';
 
+// component on line 13 & 14 needs to be uppercased
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' Component={Homepage} exact />
+        <Route path='/' Component={Homepage} exact /> 
         <Route path='/chats' Component={ChatPage}/>
       {/* <Button colorScheme='teal'>Button</Button> */}
       </Routes>
